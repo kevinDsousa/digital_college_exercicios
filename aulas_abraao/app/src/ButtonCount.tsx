196c6034fd0c51703f1e9f1a5a91b33f3bf778fb
kevin <kevindiegodasilvasousa@gmail.com>
2023-03-14 10:07:28 -0300

    alterações de estado

--- aulas_abraao/app/src/ButtonCount.tsx
+++ aulas_abraao/app/src/ButtonCount.tsx
@@ -2,10 +2,15 @@ import './ButtonCount.css'
 import { useState } from 'react'
 
 export default function ButtonCount() {
-    const [count, setCount] = useState(0)
-    return (
-        <button className='butauzin' onClick={() => setCount((count) => count + 1)}>
-          count is <span>{count}</span>
-        </button>
-    )
+
+  function handclik() {
+    setCount((c) => c + 1) // forma mais precisa de realizar a contagem
+    setCount(count + 1) // forma usual de realizar a contagem
+  }
+  const [count, setCount] = useState(0)
+  return (
+    <button className='butauzin' onClick={handclik}>
+      count is <span>{count}</span>
+    </button>
+  )
 }
