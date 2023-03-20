e5c4807cab4956d4a21bf8207602369d4101c001
kevin <kevindiegodasilvasousa@gmail.com>
2023-03-20 09:38:52 -0300

    feat: exemplos de funcoes

---
+++ Marco/uso_functions/scripts.js
@@ -0,0 +1,23 @@
+//funcao padrao
+function nome(props) {
+    // faz alguma coisa
+}
+
+//funcao anonima
+arraay.filter(function(props) {
+    // faz alguma coisa
+})
+
+//arrow function
+const nome2 = (props) => {
+    // faz alguma coisa
+    return // geralmente precisa de retorno
+}
+
+//arrow function reduzida
+const nome = (props) => algo; 
+
+//funcao autoexecutavel
+(() => {
+    // faz alguma coisa
+})()
