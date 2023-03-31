35eab39dd02baffc8624bbcefe0115fd17c954fc
kevin <kevindiegodasilvasousa@gmail.com>
2023-03-31 13:43:40 -0300

    updated jogo da velha

--- aulas_gleidson/Marco/jogo_da_velha/scripts.js
+++ aulas_gleidson/Marco/jogo_da_velha/scripts.js
@@ -1,122 +1,116 @@
-let vez = 0;
 let jogadas = 0;
+let vez = 0;
 let vitoria = false;
+let reloadGame = false;
 
 function marcarCasa() {
-    let casa = event.target; //seleciona o casa pelo click
-    if (vitoria == false) { //Verifica se a vitoria é igual a false
-        if (!casa.classList.contains("bola") && !casa.classList.contains("xis") && jogadas <= 9) { 
-            //verifica se nao existe classe bola e xis e se as jogadas chegaram a 9
-            if (vez == 0) {
-                //identifica a vez do primeiro jogador
-                casa.classList.add("bola") //adiciona a classe bola
-                casa.setAttribute("valor", 0) // seta o atritubo para zero
-                vez = 1; // passa a vez
-            } else {
-                casa.classList.add("xis") //adiciona a classe xis
-                casa.setAttribute("valor", 1) // seta o atributo xis
-                vez = 0; // passa a vez
-            }
-            jogadas++; // variavel que soma as jogadas
-        } else {
-            // caso exista a classe bola ou xis passa para segunda verificacao
-            if (jogadas == 9) {
-                // verifica se jogadas chegou a nove
-                alert("perderam otarios") // dispara alerta de empate
-                return;
-            } else {
-                // Verifica se esta jogando em um mesmo campo
-                alert('impocivel') // dispara um alerta de proibicao
-            }
-            checkVitoria(); //chama a funcao checa vitoria
-        }
+  let casa = event.target;
+  if (vitoria == false) {
+    if (
+      !casa.classList.contains("bola") &&
+      !casa.classList.contains("xis") &&
+      jogadas <= 9
+    ) {
+      if (vez == 0) {
+        casa.classList.add("bola");
+        casa.setAttribute("value", 0);
+        vez = 1;
+      } else {
+        casa.classList.add("xis");
+        casa.setAttribute("value", 1);
+        vez = 0;
+      }
+      jogadas += 1;
     } else {
-        // caso vitoria chegue a true, alerta fim de jogo
-        alert("fim de jogo")
+      if (jogadas == 9) {
+        result.classList.add("active");
+        document.querySelector("#result h1").innerHTML = "Empate";
+        return;
+      } else {
+        alert("Não pode clicar de novo");
+      }
     }
-
+    checarVitoria();
+  } else {
+    result.classList.add("active");
+    document.querySelector("#result h1").innerHTML = "Fim de jogo";
+  }
 }
-function checkVitoria() {
-    // Cria os atributos de cada caixa
-    let c1_valor = c1.getAttribute("valor");
-    let c2_valor = c2.getAttribute("valor");
-    let c3_valor = c3.getAttribute("valor");
-    let c4_valor = c4.getAttribute("valor");
-    let c5_valor = c5.getAttribute("valor");
-    let c6_valor = c6.getAttribute("valor");
-    let c7_valor = c7.getAttribute("valor");
-    let c8_valor = c8.getAttribute("valor");
-    let c9_valor = c9.getAttribute("valor");
 
-    //primeira horizontal - verifica se a condicao é verdade
-    if (c1_valor == c2_valor && c2_valor == c3_valor && c3_valor != null) {
-        resultado.classList.add("active")
-        document.querySelector("#resultado h1").innerHTML = (vez != 0) ? "Bola Venceu":"Xis Venceu";
-        vitoria = true;
+function checarVitoria() {
+  let c1_value = c1.getAttribute("value");
+  let c2_value = c2.getAttribute("value");
+  let c3_value = c3.getAttribute("value");
+  let c4_value = c4.getAttribute("value");
+  let c5_value = c5.getAttribute("value");
+  let c6_value = c6.getAttribute("value");
+  let c7_value = c7.getAttribute("value");
+  let c8_value = c8.getAttribute("value");
+  let c9_value = c9.getAttribute("value");
 
-        return;
-    }
+  /*horizontal*/
+  if (c1_value == c2_value && c2_value == c3_value && c3_value != null) {
+    resultado.classList.add("active");
+    document.querySelector("#resultado h1").innerHTML = vez != 0 ? "Bola Venceu!" : "X Venceu!";
+    vitoria = true;
+    return;
+  }
 
-    //segunda horizontal - verifica se a condicao é verdade
-    if (c4_valor == c5_valor && c5_valor == c6_valor && c6_valor != null) {
-        resultado.classList.add("active")
-        document.querySelector("#resultado h1").innerHTML = (vez != 0) ? "Bola Venceu":"Xis Venceu";
-        vitoria = true;
+  if (c4_value == c5_value && c5_value == c6_value && c6_value != null) {
+    resultado.classList.add("active");
+    document.querySelector("#resultado h1").innerHTML = vez != 0 ? "Bola Venceu!" : "X Venceu!";
+    vitoria = true;
+    return;
+  }
 
-        return;
-    }
+  if (c7_value == c8_value && c8_value == c9_value && c9_value != null) {
+    resultado.classList.add("active");
+    document.querySelector("#resultado h1").innerHTML = vez != 0 ? "Bola Venceu!" : "X Venceu!";
+    vitoria = true;
+    return;
+  }
 
-    //terceira horizontal - verifica se a condicao é verdade
-    if (c7_valor == c8_valor && c8_valor == c9_valor && c9_valor != null) {
-        resultado.classList.add("active")
-        document.querySelector("#resultado h1").innerHTML = (vez != 0) ? "Bola Venceu":"Xis Venceu";
-        vitoria = true;
+  /*vertical*/
+  if (c1_value == c4_value && c4_value == c7_value && c7_value != null) {
+    resultado.classList.add("active");
+    document.querySelector("#resultado h1").innerHTML = vez != 0 ? "Bola Venceu!" : "X Venceu!";
+    vitoria = true;
+    return;
+  }
 
-        return;
-    }
+  if (c2_value == c5_value && c5_value == c8_value && c8_value != null) {
+    resultado.classList.add("active");
+    document.querySelector("#resultado h1").innerHTML = vez != 0 ? 'Bola Venceu!' : 'X Venceu!';
+    vitoria = true;
+    return;
+  }
 
-    //primeira vertical - verifica se a condicao é verdade
-    if (c1_valor == c4_valor && c4_valor == c7_valor && c7_valor != null) {
-        resultado.classList.add("active")
-        document.querySelector("#resultado h1").innerHTML = (vez != 0) ? "Bola Venceu":"Xis Venceu";
-        vitoria = true;
+  if (c3_value == c6_value && c6_value == c9_value && c9_value != null) {
+    resultado.classList.add("active");
+    document.querySelector("#resultado h1").innerHTML = vez != 0 ? 'Bola Venceu!' : 'X Venceu!';
+    vitoria = true;
+    return;
+  }
 
-        return;
-    }
+  if (c1_value == c2_value && c2_value == c3_value && c3_value != null) {
+    resultado.classList.add("active");
+    document.querySelector("#resultado h1").innerHTML = vez != 0 ? 'Bola Venceu!' : 'X Venceu!';
+    vitoria = true;
+    return;
+  }
 
-    //segunda vertical - verifica se a condicao é verdade
-    if (c2_valor == c5_valor && c5_valor == c8_valor && c8_valor != null) {
-        resultado.classList.add("active")
-        document.querySelector("#resultado h1").innerHTML = (vez != 0) ? "Bola Venceu":"Xis Venceu";
-        vitoria = true;
+  /*diagonal*/
+  if (c1_value == c5_value && c5_value == c9_value && c9_value != null) {
+    resultado.classList.add("active");
+    document.querySelector("#resultado h1").innerHTML = vez != 0 ? 'Bola Venceu!' : 'X Venceu!';
+    vitoria = true;
+    return;
+  }
 
-        return;
-    }
-
-    //terceira verical - verifica se a condicao é verdade
-    if (c3_valor == c6_valor && c6_valor == c9_valor && c9_valor != null) {
-        resultado.classList.add("active")
-        document.querySelector("#resultado h1").innerHTML = (vez != 0) ? "Bola Venceu":"Xis Venceu";
-        vitoria = true;
-
-        return;
-    }
-
-    //primeira diagonal - verifica se a condicao é verdade
-    if (c1_valor == c5_valor && c5_valor == c9_valor && c9_valor != null) {
-        resultado.classList.add("active")
-        document.querySelector("#resultado h1").innerHTML = (vez != 0) ? "Bola Venceu":"Xis Venceu";
-        vitoria = true;
-
-        return;
-    }
-
-    //segunda diagonal - verifica se a condicao é verdade
-    if (c3_valor == c5_valor && c5_valor == c7_valor && c7_valor != null) {
-        resultado.classList.add("active")
-        document.querySelector("#resultado h1").innerHTML = (vez != 0) ? "Bola Venceu":"Xis Venceu";
-        vitoria = true;
-
-        return;
-    }
+  if (c3_value == c5_value && c5_value == c7_value && c7_value != null) {
+    resultado.classList.add("active");
+    document.querySelector("#resultado h1").innerHTML = vez != 0 ? 'Bola Venceu!' : 'X Venceu!';
+    vitoria = true;
+    return;
+  }
 }
