bd7633ed1ef070c9dab194a9cf1a1b0b600ee298
Kevin Diego <55944691+KevinDik@users.noreply.github.com>
2023-04-01 14:25:21 +0000

    estudos de requisicoes

--- aulas_ronier/aula_01.04/promices/scripts.js
+++ aulas_ronier/aula_01.04/promices/scripts.js
@@ -1,9 +1,22 @@
-let url = ('https://servicodados.ibge.gov.br/api/v1/localidades/estados/ce/municipios?orderBy=nome')
+function buscarEstados() {
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados/?orderBy=nome')
+        .then(resp => resp.json())
+        .then(data => {
+            data.forEach(item => {
+                estados.innerHTML += `<option value="${item.id}">${item.nome}</option>`
+            })
+        })
+}
+buscarEstados()
 
-fetch(url)
+estados.addEventListener('change', e => {
+    municipios.innerHTML = ''
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${e.target.value}/municipios?orderBy=nome`)
 .then(resp => (resp.json())
 .then(data => {
     data.forEach(item => {
         municipios.innerHTML += `<option value="${item.id}">${item.nome}</option>`
     })
-}))
+})
+    .catch(err => console.log(err)))
+})
