1d782ee8d62b2d1ed5a300a2c776eabe04ab1b88
Kevin Diego <55944691+KevinDik@users.noreply.github.com>
2023-04-17 12:39:18 +0000

    wip: trabalhando no aside do dashboard

--- aulas_gleidson/Abril/dashboard/js/scripts.js
+++ aulas_gleidson/Abril/dashboard/js/scripts.js
@@ -13,3 +13,16 @@ let btnAbrir = document.querySelector('.abrir-lateral');
 btnAbrir.onclick = e => {
     lateral.classList.add('active')
 }
+
+let btnFechar = document.querySelector('.fechar-lateral');
+btnFechar.onclick = e => {
+    lateral.classList.remove('active')
+}
+
+function listarCategorias() {
+    fetch('https://6439dfea90cd4ba563ef55e2.mockapi.io/api/v1/categorias')
+    .then(resp => resp.json())
+    .then(dados => {
+        
+    })
+}
