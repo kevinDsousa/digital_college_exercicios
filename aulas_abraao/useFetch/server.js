9ae2892ea6f7a20656e742d3caff6eb7133a78de
kevin <kevindiegodasilvasousa@gmail.com>
2023-05-10 09:51:44 -0300

    feat: aula de rotas

--- aulas_abraao/useFetch/server.js
+++ aulas_abraao/useFetch/server.js
@@ -1,26 +1,9 @@
-const http = require('http');
-//import http from "http";
+//const express = require('express')
+import express from 'express'
+const app = express()
 
-const PORT = 8001;
+app.get('/', function (req, res) {
+  console.log(res);
+})
 
-http.createServer(async (req, res) => {
-  console.log("Request", req);
-
-  res.writeHead(200, {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-    "Access-Control-Max-Age": 2592000,
-  })
-
-  res.end(
-    JSON.stringify(
-        {
-            "name": "Kevin diego",
-            "idade": 28,
-            "github": "https://github.com/kevinDsousa.png"
-        }
-    )
-  );
-}).listen(PORT);
-
-console.log(`Server running at http://localhost:${PORT}`);
+app.listen(3000)
